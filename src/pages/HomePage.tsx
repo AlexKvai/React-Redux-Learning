d64bd9a41bc6bf4a919a70cbff6107eecbcbc713
alex parfenov <alexp-2004@mail.ru>
2022-11-03 17:34:33 +0700

    Add hook useDebounce for input

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDebounce } from '../hooks/debounce';
 import { useSearchUsersQuery } from '../store/github/guthub.api';
 
 export const HomePage = () => {
-   const { isLoading, isError, data } = useSearchUsersQuery('alexkvai');
+   const [search, setSearch] = useState('');
+   const debounced = useDebounce(search);
+   const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
+      skip: debounced.length < 3,
+   });
+   useEffect(() => {
+      console.log(search);
+   }, [debounced]);
    return (
       <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
          {isError && (
@@ -14,6 +22,8 @@ export const HomePage = () => {
                type='text'
                className='border py-2 px-4 w-full h-[42px] mb-2'
                placeholder='Search for Github username...'
+               value={search}
+               onChange={(e) => setSearch(e.target.value)}
             />
             <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
