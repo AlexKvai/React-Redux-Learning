52f42e84993a9785706c024f6c8709eb502293e5
alex parfenov <alexp-2004@mail.ru>
2022-11-03 18:01:34 +0700

    Add useLazeGet

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useDebounce } from '../hooks/debounce';
-import { useSearchUsersQuery } from '../store/github/guthub.api';
+import {
+   useLazyGetUserReposQuery,
+   useSearchUsersQuery,
+} from '../store/github/guthub.api';
 
 export const HomePage = () => {
    const [search, setSearch] = useState('');
+   const [dropdown, setDropdown] = useState(false);
    const debounced = useDebounce(search);
    const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
       skip: debounced.length < 3,
+      refetchOnFocus: true,
    });
+   const [fetchRepos, { isLoading: areReposLoading, data: repos }] =
+      useLazyGetUserReposQuery();
    useEffect(() => {
-      console.log(search);
-   }, [debounced]);
+      setDropdown(debounced.length > 3 && data?.length! > 0);
+   }, [debounced, data]);
+
+   const clickHandler = (username: string) => {
+      fetchRepos(username);
+   };
+
    return (
       <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
          {isError && (
@@ -25,17 +37,25 @@ export const HomePage = () => {
                value={search}
                onChange={(e) => setSearch(e.target.value)}
             />
-            <ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
-               {isLoading && <p className='text-center'>Loading...</p>}
-               {data?.map((user) => (
-                  <li
-                     key={user.id}
-                     className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
-                  >
-                     {user.login}
-                  </li>
-               ))}
-            </ul>
+            {dropdown && (
+               <ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
+                  {isLoading && <p className='text-center'>Loading...</p>}
+                  {data?.map((user) => (
+                     <li
+                        onClick={() => clickHandler(user.login)}
+                        key={user.id}
+                        className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+                     >
+                        {user.login}
+                     </li>
+                  ))}
+               </ul>
+            )}
+            <div className='container'>
+               {areReposLoading && (
+                  <p className='text-center'>Repos are loading...</p>
+               )}
+            </div>
          </div>
       </div>
    );
